d5debccbd3bf6ad04ecf8a747187373f00c19621
Alexander <a.jegorov84@gmail.com>
2019-12-12 13:56:14 +0000

    finishing touches

--- src/App.java
+++ src/App.java
@@ -113,7 +113,7 @@ public class App {
                     showMenu();
                     break;
                 case 0:
-                    //finish looping is user inputs zero
+                    //finish looping if user inputs zero
                     break options;
                 default:
                     System.out.println(YELLOW + "<< check your input >>" + CLEAR);
